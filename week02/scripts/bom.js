c1b796101610a2b543c5c76ce4d3145d16a2fe87
alicemartins-hue <amartins1@byupathway.edu>
2025-05-21 18:13:43 +0100

    js dom

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,9 +3,25 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
-li.textContent = input.value;
-deleteButton.textContent = "x";
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener("click", function () {
+    if (input.value.trim() !== "") {
+
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
+        li.textContent = input.value;
+        deleteButton.textContent = "x";
+
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        
+        input.focus();
+    }
+});
+
