08e1c90f4e434dbf5b52b0dcdd5e883f5bd8da2a
alicemartins-hue <amartins1@byupathway.edu>
2025-05-27 14:28:02 +0100

    temple js hamburguer menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,14 +1,13 @@
-
 const menuButton = document.getElementById("menu-button");
 const navMenu = document.getElementById("nav-menu").querySelector("ul");
 
 menuButton.addEventListener("click", () => {
     navMenu.classList.toggle("show");
 
-   
-menuButton.textContent = navMenu.classList.contains("show") ? "✖" : "☰";
-menuButton.setAttribute(
-"aria-label",
-navMenu.classList.contains("show") ? "Fechar menu" : "Abrir menu"
+    menuButton.textContent = navMenu.classList.contains("show") ? "✖" : "☰";
+    menuButton.setAttribute(
+        "aria-label",
+        navMenu.classList.contains("show") ? "Fechar menu" : "Abrir menu"
     );
 });
+
